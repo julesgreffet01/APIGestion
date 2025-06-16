2c5d68927f7f26ca94a19afd2e2225d9606adc58
Jules Greffet <julesgreffet@gmail.com>
2025-06-16 12:37:55 +0200

    jules

---
+++ src/plugins/response.ts
@@ -0,0 +1,25 @@
+import fp from 'fastify-plugin'
+import {FastifyInstance, FastifyReply} from 'fastify'
+
+export default fp(async function (fastify: FastifyInstance) {
+    fastify.decorateReply('apiResponse', async function (reply: FastifyReply, statusCode: number, data = null): Promise<FastifyReply> {
+        const defaultMessages: Record<number, string> = {
+            200: 'Succès',
+            201: 'Créé avec succès',
+            400: 'Requête invalide',
+            401: 'Non autorisé',
+            403: 'Accès interdit',
+            404: 'Ressource introuvable',
+            500: 'Erreur interne du serveur',
+        }
+
+        const response = {
+            message: defaultMessages[statusCode] || 'requete valide',
+            data: ''
+        }
+        if (data !== null) {
+            response.data = data
+        }
+        return reply.code(statusCode).send(response)
+    });
+});
